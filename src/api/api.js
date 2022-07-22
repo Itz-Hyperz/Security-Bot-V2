70db8b9c1b238afdbc0fb4b1514a94b254b34b2e
Itz-Hyperz <loganfgross@gmail.com>
2022-07-22 08:21:56 -0700

    Added FirewallGG API Endpoint Natively.

--- src/api/api.js
+++ src/api/api.js
@@ -253,6 +253,32 @@ function apistart(client, con) {
                 }
             });
         })
+        
+        app.get('/firewallgg/checkuser/:userid', async function(req, res) {
+            res.set('Access-Control-Allow-Origin', '*');
+            if(!req?.params?.userid) return res.redirect('/');
+            req.params.userid == req.params.userid.replaceAll('`', '').replaceAll('"', '');
+            await con.query(`SELECT * FROM bans WHERE userid="${req?.params?.userid}" AND active="true"`, async (err, row) => {
+                if(err) throw err;
+                if(!row[0]) {
+                    // If the user is not banned
+                    let json_ = {
+                        "active": false, // This means that the user is not banned
+                    }
+                    return res.type('json').send(JSON.stringify(json_, null, 4) + '\n');
+                } else {
+                    // If the user is banned
+                    let json_ = {
+                        "active": true,
+                        "userid": row[0].userid,
+                        "reason": row[0].reason,
+                        "proof": row[0].proof || 'None provided...',
+                        "time": row[0].timeofban
+                    }
+                    return res.type('json').send(JSON.stringify(json_, null, 4) + '\n');
+                };
+            });
+        });
 
         app.get(`/hyperz`, async function(req, res) {
             res.redirect('https://hyperz.net');
